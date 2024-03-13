c557195c96b08a9f4e839b3bfb2238f79650e1eb
Pandora <imcathalat@gmail.com>
2024-03-13 16:36:34 -0300

    adding first ajax requisition

--- src/posts/static/posts/main.js
+++ src/posts/static/posts/main.js
@@ -1,5 +1,14 @@
-console.log("Hello World")
 
-const helloWolrdBox = document.getElementById('hello-world')
+const helloWorldBox = document.getElementById('hello-world')
 
-helloWorldBox.innerHTML = "hello <br>World</br>"
+$.ajax({
+    type: 'GET',
+    url: '/hello-world/',
+    success: function(response){
+        console.log('succes', response.text)
+        helloWorldBox.textContent = response.text
+    },
+    error: function(error) {
+        console.log('error', error)
+    }
+});
